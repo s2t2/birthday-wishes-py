c9cb47d42b893f6aef80ac5dd6a46a95cde113c7
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-07 17:16:41 -0500

    Setup example script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Birthday Wishes - Python
+
+## Usage
+
+Run the notification script:
+
+```sh
+python app/notifier.py
+```
---
+++ app/notifier.py
@@ -0,0 +1,5 @@
+
+
+if __name__ == "__main__":
+    print("DETECTING UPCOMING BIRTHDAYS...")
+    print("SENDING NOTIFICATION...")
