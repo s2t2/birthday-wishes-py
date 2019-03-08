9eb8f1d4e50c9845136ea55b7915f1e7db63b8d0
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-08 15:35:18 -0500

    Send SMS via Twilio service

--- CREDITS.md
+++ CREDITS.md
@@ -13,3 +13,18 @@ First, sign up for a free account, then verify account. Then create an API Key w
 ## Pretty Printing
 
   + https://docs.python.org/3/library/pprint.html
+
+## Twilio
+
+  + https://www.twilio.com/docs/libraries/python
+  + https://github.com/twilio/twilio-python
+  + https://www.twilio.com/console/gate
+  + https://www.twilio.com/docs/errors/21608
+
+For some recipient phone numbers, seeing error message "Unable to create record: The number  is unverified. Trial accounts cannot send messages to unverified numbers; verify  at twilio.com/user/account/phone-numbers/verified, or purchase a Twilio number to send messages to unverified numbers."
+
+... "To prevent spam, trial accounts can only send messages to phone numbers you've verified with Twilio. You must upgrade your account to remove this restriction."
+
+It seems in order to send an SMS to a phone number, you first need to "verify" that number. The process involves sending an SMS to the requested number, and then entering a code sent to that number. So if you want to send to someone else, you need to coordinate with them beforehand.
+
+  + https://www.twilio.com/console/phone-numbers/verified
--- app/notifier.py
+++ app/notifier.py
@@ -1,10 +1,11 @@
 
 import os
-#import pprint
+import pprint
 
 from dotenv import load_dotenv
 import sendgrid
 from sendgrid.helpers.mail import *
+from twilio.rest import Client
 
 load_dotenv()
 
@@ -12,10 +13,15 @@ SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please specify env 
 SENDER_EMAIL_ADDRESS = os.environ.get("SENDER_EMAIL_ADDRESS", "OOPS, please specify env var called 'SENDER_EMAIL_ADDRESS'")
 RECIPIENT_EMAIL_ADDRESS = os.environ.get("RECIPIENT_EMAIL_ADDRESS", "OOPS, please specify env var called 'RECIPIENT_EMAIL_ADDRESS'")
 
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID", "OOPS, please specify env var called 'TWILIO_ACCOUNT_SID'")
+TWILIO_AUTH_TOKEN  = os.environ.get("TWILIO_AUTH_TOKEN", "OOPS, please specify env var called 'TWILIO_AUTH_TOKEN'")
+SENDER_SMS  = os.environ.get("SENDER_SMS", "OOPS, please specify env var called 'SENDER_SMS'")
+RECIPIENT_SMS  = os.environ.get("RECIPIENT_SMS", "OOPS, please specify env var called 'RECIPIENT_SMS'")
+
 def upcoming_birthdays():
     return []
 
-# see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/sendgrid.md
+# adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/sendgrid.md
 def send_email():
     sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
 
@@ -29,16 +35,35 @@ def send_email():
     response = sg.client.mail.send.post(request_body=mail.get())
     return response
 
+# adapted from: https://www.twilio.com/docs/libraries/python
+def send_sms():
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    content = "FYI - Callie Cousin's birthday is coming up on June 5th. You can send her a text at: 123.456.7890"
+    message = client.messages.create(to=RECIPIENT_SMS, from_=SENDER_SMS, body=content)
+    return message #> <class 'twilio.rest.api.v2010.account.message.MessageInstance'>
+
 if __name__ == "__main__":
+    pp = pprint.PrettyPrinter(indent=4)
+
     print("DETECTING UPCOMING BIRTHDAYS...")
     upcoming_birthdays()
 
     print("SENDING NOTIFICATIONS...")
-    response = send_email()
 
-    #pp = pprint.PrettyPrinter(indent=4)
-    print("RESPONSE: ", type(response))
-    print("STATUS:", response.status_code)
+    ## EMAIL
+    #
+    #response = send_email()
+    #print("RESPONSE: ", type(response))
+    #print("STATUS:", response.status_code)
     #print("HEADERS:")
     #pp.pprint(dict(response.headers))
     #print("BODY:", response.body)
+
+    # SMS
+
+    sms_response = send_sms()
+    print("FROM:", sms_response.from_)
+    print("TO:", sms_response.to)
+    print("BODY:", sms_response.body)
+    pp.pprint(dict(sms_response._properties))
